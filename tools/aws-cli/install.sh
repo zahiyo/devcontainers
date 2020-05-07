084dcdc1d6c963f12e6d0c75af09eb0f9a427922
Younes Zahidi <12915382+zahiyo@users.noreply.github.com>
2020-05-07 15:00:27 -0400

    Copy aws-cli public key from build context

--- tools/aws-cli/install.sh
+++ tools/aws-cli/install.sh
@@ -2,17 +2,16 @@
 
 set -e
 
+PUBLIC_KEY=$1
+
 # Download the zip file
 curl -o awscliv2.zip https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip
 
 # Download the AWS CLI signature file
 curl -o awscliv2.sig https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip.sig
 
-# Download the public key
-curl -o awscliv2.public-key https://raw.githubusercontent.com/zahiyo/devcontainers/master/tools/aws-cli/public-key
-
 # Import the AWS CLI public key
-gpg --import awscliv2.public-key
+gpg --import $PUBLIC_KEY
 
 # Verify the signature
 gpg --verify awscliv2.sig awscliv2.zip
